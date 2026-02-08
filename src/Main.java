ef5b0d62728675022ecf04a21a4b6c9571d26861
clementange <kengneangeo5@gmail.com>
2026-02-08 16:41:50 +0100

    ajout de tous les autres fichiers du dossiers

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner Scanner = new Scanner(System.in);
+        String [] tableauValeur = new String  [5];
+        tableauValeur[0]= "5";
+        System.out.println(tableauValeur.length);
+
+    }
+}
